575faecd039d9211a4dd0897514cf4821071725a
NURLAN <regalix@gmail.com>
2018-12-16 13:27:46 +0600

    16.12.2018

--- src/main/java/kg/gov/mf/loan/core/dao/LoggerDao.java
+++
@@ -1,8 +0,0 @@
-package kg.gov.mf.loan.core.dao;
-
-
-import kg.gov.mf.loan.core.model.MFLog;
-
-public interface LoggerDao extends GenericDao<MFLog>
-{
-}
--- src/main/java/kg/gov/mf/loan/core/service/LoggerService.java
+++
@@ -1,8 +0,0 @@
-package kg.gov.mf.loan.core.service;
-
-import kg.gov.mf.loan.core.model.MFLog;
-
-public interface LoggerService extends GenericService<MFLog>
-{
-    void addLog(String user, String entity, String action, String ip);
-}
